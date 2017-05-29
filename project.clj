9c2e25e8311f77324b19fc48d51f3383a772aaa5
Brennan Chesley <holtchesley@example.com>
2017-05-29 14:57:40 -0400

    License and url removed from project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject set-invaders "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A combination of the boardgame Set, and videogame Space Invaders"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.6.0"]])
